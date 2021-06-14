3704bef711403117479e468f1ece403072b1aba9
Lalita Lowphansirikul <lalita.lowphansirikul@gmail.com>
2021-06-14 08:30:59 +0700

    Save file as best

--- train_dense_encoder.py
+++ train_dense_encoder.py
@@ -234,7 +234,7 @@ class BiEncoderTrainer(object):
                 self.best_validation_result = validation_loss
                 self.best_cp_name = cp_name
                 logger.info("New Best validation checkpoint %s", cp_name)
-                self._save_checkpoint(scheduler, f'{epoch}.best', iteration)
+                self._save_checkpoint(scheduler, f'best', iteration)
 
     def validate_nll(self) -> float:
         logger.info("NLL validation ...")
