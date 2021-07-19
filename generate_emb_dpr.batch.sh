7d325c74c5a1644490008c240823126803bcacc1
Lalita Lowphansirikul <lalita.lowphansirikul@gmail.com>
2021-07-19 20:01:39 +0700

    Change variable name

--- generate_emb_dpr.batch.sh
+++ generate_emb_dpr.batch.sh
@@ -26,7 +26,7 @@ do
                     ctx_src=dpr_thwiki-20171217_split-${CHUNK_SIZE} \
                     shard_id=0 num_shards=1 \
                     batch_size=680 \
-                    out_file=/workspace/embed/dpr_wiki/embed.batched.${EXP_NAME}_best@ |& tee -a /workspace/logs/generate_dense_embeddings/${RESULT_NAME}.log
+                    out_file=/workspace/embed/dpr_wiki/embed.batched.${EXP_NAME}_best@ |& tee -a /workspace/logs/generate_dense_embeddings/${EXP_NAME}.log
             fi
 
             CUDA_VISIBLE_DEVICE=1 python dense_retriever.py \
